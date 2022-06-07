92c9d2a41ad3e30b0beb827a9749fee28291f585
Maneesh-Arora <maneesh2000@yahoo.com>
2022-06-07 17:05:33 +0530

    Initial DI Project Add

---
+++ src/main/java/guru/springframework/sfgdi/controllers/PropertyInjectedController.java
@@ -0,0 +1,8 @@
+package guru.springframework.sfgdi.controllers;
+
+import guru.springframework.sfgdi.services.GreetingService;
+
+public class PropertyInjectedController {
+    public GreetingService greetingService;
+
+}
