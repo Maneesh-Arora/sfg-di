47337f731ad443f1256095a4afddf4896be9489c
Maneesh-Arora <106664702+Maneesh-Arora@users.noreply.github.com>
2022-06-08 00:02:25 +0530

    Update ClaimApprovalManagerTest.java
    OpenCLosedPrinciple

--- src/test/java/guru/springframework/sfgdi/openclosed/ClaimApprovalManagerTest.java
+++ src/test/java/guru/springframework/sfgdi/openclosed/ClaimApprovalManagerTest.java
@@ -8,5 +8,17 @@ class ClaimApprovalManagerTest {
 
     @Test
     void processClaim() {
+      HealthInsuranceSurveyor healthInsuranceSurveyor = new HealthInsuranceSurveyor();
+ 
+      ClaimApprovalManager claim1 = new ClaimApprovalManager();
+ 
+      claim1.processClaim(healthInsuranceSurveyor);
+ 
+      VehicleInsuranceSurveyor vehicleInsuranceSurveyor = new VehicleInsuranceSurveyor();
+ 
+      ClaimApprovalManager claim2 = new ClaimApprovalManager();
+ 
+      claim2.processClaim(vehicleInsuranceSurveyor);
+    }
     }
 }
