21d6dbc73771077d51281e334b19f6f6cbc21fe6
Maneesh-Arora <106664702+Maneesh-Arora@users.noreply.github.com>
2022-06-07 23:50:33 +0530

    Create HealthInsuranceSurveyor.java
    OpenClosePrinciple

---
+++ src/main/java/guru/springframework/sfgdi/HealthInsuranceSurveyor.java
@@ -0,0 +1,8 @@
+package guru.springframework.blog.openclosedprinciple;
+public class HealthInsuranceSurveyor{
+    public boolean isValidClaim(){
+        System.out.println("HealthInsuranceSurveyor: Validating health insurance claim...");
+        /*Logic to validate health insurance claims*/
+        return true;
+    }
+}
