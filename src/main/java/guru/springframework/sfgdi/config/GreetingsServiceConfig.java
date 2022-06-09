f1a0c7d6e9b5d60f8177cb98943231bc33a047e2
Maneesh-Arora <maneesh2000@yahoo.com>
2022-06-09 15:54:38 +0530

    user yaml files instead of properties files

--- src/main/java/guru/springframework/sfgdi/config/GreetingsServiceConfig.java
+++ src/main/java/guru/springframework/sfgdi/config/GreetingsServiceConfig.java
@@ -8,7 +8,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.*;
 
-@PropertySource("classpath:datasource.properties")
 @Configuration
 public class GreetingsServiceConfig {
 
